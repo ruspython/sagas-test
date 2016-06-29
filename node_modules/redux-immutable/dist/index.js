996266a3ed5edc2a1e2fc97e60ec82339d9da46a
vlad <ruspython@gmail.com>
2016-06-29 14:51:07 +0300

    Initial commit

---
+++ node_modules/redux-immutable/dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.combineReducers = undefined;
+
+var _combineReducers = require('./combineReducers');
+
+var _combineReducers2 = _interopRequireDefault(_combineReducers);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.combineReducers = _combineReducers2.default;
+//# sourceMappingURL=index.js.map
